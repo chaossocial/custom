f2947436ee1566e886a925884b033a14ea6d2dd4
Tobias Kunze <r@rixx.de>
2022-11-06 11:20:49 +0100

    [scripts] Improve format, add config yoinking

--- scripts/registration-limit.sh
+++ scripts/registration-limit.sh
@@ -2,10 +2,18 @@
 set -e
 
 MAX_USERS=42
+RAILS_ENV=production
+TOOTCTL="${HOME}/live/bin/tootctl"
 
-min_invite_role=$(/usr/bin/psql mastodon -t -c "select value from settings where var like 'min_invite_role';")
-today=$(/usr/bin/psql mastodon -t -c "select count(*) from users where users.created_at::date = NOW()::date;")
-echo "$today"
+# Config grabbing yoinked from https://codeberg.org/Windfluechter/check_mastodon.sh/src/branch/main/check_mastodon.sh
+if [ ! -e ${HOME}/bin/registration_limit.conf ]; then
+	grep -e ^"DB_" -e ^"LOCAL_DOMAIN" ${HOME}/live/.env.production > ${HOME}/bin/cleanup-mastodon-users.conf
+fi
+
+
+INVITE_ROLE=$(/usr/bin/psql mastodon -t -c "SELECT value FROM settings WHERE var LIKE 'min_invite_role';")
+USERS_TODAY=$(/usr/bin/psql mastodon -t -c "SELECT count(*) FROM users WHERE users.created_at::date = NOW()::date AND disabled=false;")
+echo "$USERS_TODAY"
 
 # TODO make /usr/bin/psql use remote access, either directly or by maybe using `rake exec dbshell`
 
@@ -17,13 +25,13 @@ echo "$today"
 # output: 1) "cache:rails_settings_cached/8cf69c0340d0506f5783bd343106ebea/min_invite_role"
 # we need to cut out only the "1)"
 
-if [ "$today" -lt "$MAX_USERS" ] && [[ "$min_invite_role" == *"admin"* ]]; then
+if [ "$USERS_TODAY" -lt "$MAX_USERS" ] && [[ "$INVITE_ROLE" == *"admin"* ]]; then
 	# Few sign-ups but admin mode, date rollover
 	echo "opening registrations and restoring invites"
 	/usr/bin/psql mastodon -t -c "UPDATE invites SET expires_at=TO_TIMESTAMP(comment, 'YYYY-MM-DD HH24:MI:SS.US'), comment='' WHERE expires_at='1999-09-09 00:00:00';"
     /usr/bin/psql mastodon -t -c "UPDATE settings SET value = REPLACE(value, 'admin', 'user'), updated_at=NOW() WHERE var = 'min_invite_role';"
     #redis-cli -h localhost -p 6379 set "cache:rails_settings_cached/8cf69c0340d0506f5783bd343106ebea/min_invite_role" "\x04\bo: ActiveSupport::Cache::Entry\t:\x0b@valueI\"\tuser\x06:\x06ET:\r@version0:\x10@created_atf\x171667700951.1239355:\x10@expires_inf\b6e2" PX 600000
-elif [ "$today" -gt $MAX_USERS ] && [[ "$min_invite_role" == *"user"* ]] ; then
+elif [ "$USERS_TODAY" -gt $MAX_USERS ] && [[ "$INVITE_ROLE" == *"user"* ]] ; then
 	# Too many sign-ups but still in user mode
 	echo "closing registrations and disabling invites"
 	/usr/bin/psql mastodon -t -c "UPDATE invites SET comment=expires_at, expires_at='1999-09-09 00:00:00' WHERE (max_uses IS NULL OR uses < max_uses) AND (expires_at IS NULL OR expires_at > now());"
